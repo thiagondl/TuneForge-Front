f4c8680ac042e22ab051eac12e8442ab598a2a70
thiagondl <thiago.lima@ccc.ufcg.edu.br>
2023-04-01 15:43:34 -0300

    add key and lang

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,4 @@
-import { Header, Segment, Grid, Input, Divider, Button } from 'semantic-ui-react'
+import { Header, Segment, Grid, Input, Divider, Button, Select } from 'semantic-ui-react'
 import React, { useState, useEffect } from 'react';
 import Lyric from './Lyric';
 
@@ -10,6 +10,8 @@ function Home() {
   const [theme, setTheme] = useState("");
   const [style, setStyle] = useState("");
   const [mood, setMood] = useState("");
+  const [key, setKey] = useState("");
+  const [language, setLanguage] = useState("");
 
   useEffect(() => {
     makeRequest();
@@ -54,13 +56,26 @@ function Home() {
     setMood(event.target.value)
   }
 
+  function updateKey(event) {
+    setKey(event.target.value)
+  }
+
+  function updateLanguage(_, { value }) {
+    setLanguage(value)
+  }
+
+  const optionsLanguage = [
+    { key: 'e', text: 'English', value: 'en' },
+    { key: 'p', text: 'Portuguese', value: 'pt-br' },
+  ]
+
   return (
     <>
       <Segment basic inverted color='orange' style={{padding: '1.5%'}}>
         <Header textAlign='center' as='h1'>Tune Forge</Header>
       </Segment>
       <Grid style={{margin: '0% 5%'}}>
-        <Grid.Row textAlign='left' columns={5}>
+        <Grid.Row textAlign='left' columns={4}>
           <Grid.Column>
             Genre:
             <Input fluid onChange={updateGenre} placeholder='Ex: Rock' />
@@ -76,6 +91,16 @@ function Home() {
           <Grid.Column>
             Mood:
             <Input fluid onChange={updateMood} placeholder='Ex: Happy' />
+          </Grid.Column>
+        </Grid.Row>
+        <Grid.Row textAlign='left' columns={3}>
+          <Grid.Column>
+            Key:
+            <Input fluid onChange={updateKey} placeholder='Ex: C#' />
+          </Grid.Column>
+          <Grid.Column>
+            Language:
+            <Select fluid onChange={updateLanguage} options={optionsLanguage} placeholder='English' />
           </Grid.Column>
           <Grid.Column>
             <div style={{visibility: 'hidden'}}>-</div>
