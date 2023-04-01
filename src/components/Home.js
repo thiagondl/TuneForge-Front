454efde465f0dd601676eb7b01d706988c0fa871
thiagondl <thiago.lima@ccc.ufcg.edu.br>
2023-04-01 13:20:34 -0300

    v1

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,17 +1,29 @@
+import { Header, Segment, Grid, Input } from 'semantic-ui-react'
+
 function Home() {
+
   return (
-  <div className="home-screen">
-    <nav className="navbar">
-      <ul>
-        <li><a href="/">Home</a></li>
-        <li><a href="/about">About</a></li>
-        <li><a href="/contact">Contact</a></li>
-      </ul>
-    </nav>
-    <h1>Welcome to my App</h1>
-    <p>This is the home screen of my app.</p>
-    <button>Get Started</button>
-  </div>
+    <>
+      <Segment inverted color='orange'>
+        <Header textAlign='center' as='h1'>Tune Forge</Header>
+      </Segment>
+      <Grid>
+        <Grid.Row style={{backgroundColor: 'black'}} columns={4}>
+          <Grid.Column width={4}>
+            <Input fluid placeholder='Genre...' />
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Input fluid placeholder='Theme...' />
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Input fluid placeholder='Syle...' />
+          </Grid.Column>
+          <Grid.Column width={4}>
+            <Input fluid placeholder='Mood...' />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </>
   );
 }
 
